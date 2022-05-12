df20c2d0b64b0e4833487fe5e349fc88c288628e
Евгений Борщ <b0980652405@gmail.com>
2022-05-12 10:47:27 +0300

    schedule run

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        PusherServer::class
+        //
     ];
 
     /**
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('watch:run')->everyMinute();
     }
 
     /**
